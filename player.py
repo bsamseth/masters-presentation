6bce91080e79ced593e21dca83ea1495bb8c7b28
Bendik Samseth <b.samseth@gmail.com>
2019-09-17 01:30:56 +0200

    Black player

--- player.py
+++ player.py
@@ -19,32 +19,36 @@ while i < len(scenes):
     paused = False
     scene = cv2.VideoCapture(scenes[i])
     prev, prev_stop_frame = 0, 0
-    print('showing scene', i)
+    print("showing scene", i)
 
     frame_count = 0
     while scene.isOpened():
         if paused:
             key = cv2.waitKey(0) & 0xFF  # Wait indefinitely
-            if key == ord(' '):
+            if key == ord(" "):
                 paused = not paused
                 prev_stop_frame = frame_count
                 continue
             elif key == 8:  # 8 == ascii for backspace.
                 i = max(-1, i - 2)
                 break
-            elif key == ord('q'):
+            elif key == ord("q"):
                 i = len(scenes)
                 break
 
         ret, frame = scene.read()
         if ret:
-            if not paused and frame_count - prev_stop_frame > FPS / 2 and np.all(frame == prev):
+            if (
+                not paused
+                and frame_count - prev_stop_frame > FPS / 2
+                and np.all(frame == prev)
+            ):
                 paused = True
                 prev_stop_frame = frame_count
             prev = frame
 
             delta = time.time() - t0
-            if cv2.waitKey(max(1, int(1000 * (1 / FPS - delta))))  & 0xFF == ord(' '):
+            if cv2.waitKey(max(1, int(1000 * (1 / FPS - delta)))) & 0xFF == ord(" "):
                 paused = not paused
 
             cv2.imshow("Frame", frame)
